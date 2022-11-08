f7e0a934cbe92b03256131ebbcaac3975f4c8ff7
tanselbay1 <tanselbay1@hotmail.com>
2022-11-08 13:47:36 +0700

    Initialize app structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./assets/style.css";
+import data from "./data/todos.json";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello TODO</h1>
+      <ul className="list">
+        {data.map((todo) => {
+          return <li>{todo.todo}</li>;
+        })}
+      </ul>
     </div>
   );
 }
